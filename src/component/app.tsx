096893e39ba5bdbaa0e5fad4424ffe622d221bea
Jimin Park <pjm950@gmail.com>
2025-04-18 22:56:23 +0900

    Added basic description.

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -5,6 +5,10 @@ import { Pipeline } from "./pipe";
 export function App() {
     return <div className="sp-app">
         <Pipeline />
+        <p style="margin-top: 1em; padding: .5em 1em;">
+            StringPipe is an easy-to-use tool, supporting both desktop and mobile, for processing strings and bytes in a type-safe manner.<br/>
+            StringPipe is currently work-in-progress, and many features are planned.
+        </p>
     </div>;
 }
 
