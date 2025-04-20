ea1438c3585c803af6fe8e211af2b24cc17a3798
Jimin Park <pjm950@gmail.com>
2025-04-20 19:46:23 +0900

    Added copy button.

--- src/component/data-view/string-view.tsx
+++ src/component/data-view/string-view.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "preact/hooks";
 import "./string-view.css";
 
 export interface StringViewProps {
@@ -5,5 +6,10 @@ export interface StringViewProps {
 }
 
 export function StringView({value}: StringViewProps) {
-    return <div class="sp-string-view">{value}</div>;
+    const onClickCopy = useCallback(async() => {
+        await navigator.clipboard.writeText(value);
+        alert("Copied to clipboard!");
+    }, [value]);
+
+    return <div class="sp-string-view">{value}<button class="sp-string-view-copy" onClick={onClickCopy}>Copy</button></div>;
 }
