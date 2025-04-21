6aeef5b5f75d7a0b22400387ca3854152472c5ed
Jimin Park <pjm950@gmail.com>
2025-04-21 18:10:54 +0900

    Removed initial pipe.

--- src/component/pipe/pipeline/state.ts
+++ src/component/pipe/pipeline/state.ts
@@ -2,7 +2,6 @@ import { uuidv4 } from "@/util";
 
 import type { DataType, PipeDefinition } from "../type";
 import { StringInputPipe } from "../input";
-import { ToIntListPipe } from "../literal/int-list";
 
 export interface PipeState {
     id: string;
@@ -13,6 +12,5 @@ export interface PipeState {
 export function createDefaultPipeStates(): PipeState[] {
     return [
         { id: uuidv4(), pipe_def: StringInputPipe, output: "" },
-        { id: uuidv4(), pipe_def: ToIntListPipe, output: "" },
     ];
 }
